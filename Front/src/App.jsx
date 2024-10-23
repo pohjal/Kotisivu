dfdf4b6a7163a905e6412b97c22805025934ff34
Pohja Leevi <leevi.pohja@procemex.com>
2024-10-23 22:00:47 +0300

    runko

---
+++ Front/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+const Kissa = () => {
+  return (
+    <div>
+      <h1>Hello, Kissa Component!</h1>
+    </div>
+  );
+};
+
+
+function App() {
+
+  return (
+    <>
+    <Kissa/>
+  
+    </>
+  )
+}
+
+export default App
