9020199a5aab762120cfe250a7f46108019d5965
Pohja Leevi <leevi.pohja@procemex.com>
2024-11-02 00:27:36 +0200

    etusivu ja sidebar päivitys

--- Front/src/components/Sidebar.jsx
+++ Front/src/components/Sidebar.jsx
@@ -1,5 +1,4 @@
-// Sidebar.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Sidebar.css';
 
 const Sidebar = () => {
@@ -9,9 +8,41 @@ const Sidebar = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    let resizeTimer;
+
+    const handleResize = () => {
+      // Add no-transition class to disable animations during resizing
+      document.body.classList.add('no-transition');
+      
+      // Clear the timer if resizing is still happening
+      clearTimeout(resizeTimer);
+      
+      // Set a timer to remove the class after resizing stops
+      resizeTimer = setTimeout(() => {
+        document.body.classList.remove('no-transition');
+        // Optionally close sidebar when resizing stops if the screen width is small
+        if (window.innerWidth < 681) {
+          setIsOpen(false);
+        }
+      }, 300);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      clearTimeout(resizeTimer);
+    };
+  }, []);
+
   return (
     <div className="sidebar-menu">
-      <div className={`hamburger-icon ${isOpen ? 'open' : ''}`} onClick={toggleSidebar}>
+      <div
+        className={`hamburger-icon ${isOpen ? 'open' : ''}`}
+        onClick={toggleSidebar}
+      >
         <div className="bar"></div>
         <div className="bar"></div>
         <div className="bar"></div>
